8a1548f3b57fe49f31d8534fb834ffe666c00b27
ardhifaqih <ardhifq10@gmail.com>
2022-03-24 09:10:25 +0700

    argumen default fungsi

---
+++ src/main/kotlin/38_Argumen Default Fungsi.kt
@@ -0,0 +1,15 @@
+//Parameter fungsi bisa memiliki nilai default yang membolehkan pemanggil fungsi untuk menghilangkan beberapa argumen ketika dipanggil.
+fun main(args: Array<String>) {
+//panggil tanpa argumen
+    connectToDb()
+    println()
+//panggil dengan 2 argumen
+    connectToDb("sqlserver", "depandi")
+}
+fun connectToDb(hostname: String = "localhost",
+                username: String = "mysql",
+                password:String = "secret") {
+    println("hostname : $hostname")
+    println("username : $username")
+    println("password : $password")
+}
